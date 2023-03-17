239a15bfc83f7884b542006bcccaeccab36ed2cd
WennWorks <108010379+WennWorks@users.noreply.github.com>
2023-03-17 15:54:38 +0000

    new

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const isPowerOfTwo = (n) => {
-  
+  if (n < 1) {
+    return false;
+  }
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n = n / 2;
+  }
+  return true;
 }
 
-console.log(isPowerOfTwo(1));
-console.log(isPowerOfTwo(2));
-console.log(isPowerOfTwo(5));
+console.log(isPowerOfTwo(1)); // true
+console.log(isPowerOfTwo(2)); // true
+console.log(isPowerOfTwo(5));  // false
